5bbf033a83b4f1175612b8738ae7d862288252ba
Vijay kumar <vkumar3@inspirebrands.com>
2023-04-01 14:07:10 +0530

    fixed issue

--- controllers/user.request.controller.js
+++ controllers/user.request.controller.js
@@ -15,14 +15,12 @@ const saveUserRequest = async (req, res) => {
     } else {
       result = await model.save({mobile: mobile, name: name, message: message});
     }
-    console.log('doc-received---', JSON.stringify(result));
     return res.send({
       data: result,
       message: "Request saved successfully",
       status: 200,
     });
   } catch (error) {
-    console.log('error-in-request---', error?.message);
     if (error.message) {
       return res.status(400).json({ error: error.message });
     } else {
@@ -46,9 +44,6 @@ const getAllUserRequest = async (req, res) => {
 const appendUserRequestReplies = async (req, res) => {
   try {
     const { requestId } = req.params;
-    console.log('requestId---', requestId);
-    console.log('req.body.message---', req.body.message);
-
     const instance = await UserRequestModel.updateOne(
       { _id: requestId },
       {
@@ -56,7 +51,6 @@ const appendUserRequestReplies = async (req, res) => {
       }
     );
     if (instance.modifiedCount) {
-      console.log('if---instance.modifiedCount---', instance.modifiedCount);
       return res.sendStatus(200);
     } else {
       res.status(404).json({ error: "not found" });
