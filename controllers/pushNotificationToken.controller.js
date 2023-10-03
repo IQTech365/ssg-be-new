db17b18b4dfb56627c9266a45fee65064d73942c
Irfan <irfanrosmok786@gmail.com>
2023-10-04 00:00:53 +0530

    Adding mobile number to create token

--- controllers/pushNotificationToken.controller.js
+++ controllers/pushNotificationToken.controller.js
@@ -3,24 +3,31 @@ const admin = require("firebase-admin");
 
 const createNotificationToken = async (req, res) => {
     try {
-        const { token } = req.body;
+        const { token, mobile } = req.body;
         const data = await PushNotificationTokenModel.find({});
-        data && data.map(async data => {
-            if (data.token != token) {
-                const notificationToken = new PushNotificationTokenModel({ token });
-                const doc = await notificationToken.save();
-                res.send({ token: doc.token });
-            } else {
-                res.status(400).json({ error: "Identical token" });
+
+        let tokenExists = false;
+
+        for (const item of data) {
+            if (item.token === token) {
+                tokenExists = true;
+                break;
             }
-        })
+        }
 
-
+        if (!tokenExists) {
+            const notificationToken = new PushNotificationTokenModel({ token, mobile });
+            const doc = await notificationToken.save();
+            res.send({ token: doc.token, mobile: doc.mobile });
+        } else {
+            res.status(400).json({ error: "Identical token" });
+        }
     } catch (error) {
         if (error.message) {
             res.status(400).json({ error: error.message });
+        } else {
+            res.status(400).json({ error: "error" });
         }
-        res.status(400).json({ error: "error" });
     }
 };
 
